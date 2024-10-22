fb6e203d7a67e93381e5f23521a729a81004f03f
Tiago Ferreira <tiago.ferreira@it-eam.com>
2024-10-22 19:15:34 -0300

    Add TiagoApplication | Update pom

---
+++ tiago/src/main/java/br/edu/infnet/tiago/TiagoApplication.java
@@ -0,0 +1,12 @@
+package br.edu.infnet.tiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiagoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiagoApplication.class, args);
+    }
+}
